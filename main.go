484dd698a4fcbe4238dbcb2c09eb5c21e70f1adf
Villena Guillaume <contact@guillaumevillena.fr>
2020-09-22 22:40:08 +0200

    Update

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var (
 	excludeKeyString *[]string
 	upscPath         *string
 
-	prometheusMetrics map[string]map[string]prometheus.Gauge
+	prometheusMetrics map[string]prometheus.GaugeVec
 )
 
 func main() {
@@ -32,7 +32,7 @@ func main() {
 	// Create string flag
 	upsList = parser.StringList("u", "ups", &argparse.Options{Required: true, Help: "List of Ups name"})
 	excludeKeyString = parser.StringList("e", "exclude", &argparse.Options{Required: false, Help: "Do not export Key"})
-	upscPath = parser.String("e", "executable", &argparse.Options{Required: false, Help: "Path to the executable of upsc", Default: "upsc"})
+	upscPath = parser.String("b", "binary", &argparse.Options{Required: false, Help: "Path to the executable of upsc", Default: "upsc"})
 	var excludeKeyStringRegex = parser.StringList("E", "exclude-regex", &argparse.Options{Required: false, Help: "Do not export Key using Regex"})
 	var port = parser.Int("p", "port", &argparse.Options{Required: false, Help: "Port Number", Default: 8081})
 	var host = parser.String("H", "host", &argparse.Options{Required: false, Help: "Define the host address for the web server", Default: "127.0.0.1"})
@@ -43,6 +43,7 @@ func main() {
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
+		return
 	}
 
 	//Compile regex given at input
@@ -57,90 +58,104 @@ func main() {
 	log.Info("The folowing keys are ignored: ", *excludeKeyString)
 	log.Info("The folling regex will exclude keys: ", *excludeKeyStringRegex)
 
+	prometheusMetrics = map[string]prometheus.GaugeVec{}
+
+	c := make(chan string)
+	go processGaugeCreation(c)
+
 	for _, upsName := range *upsList {
-		sampleUps(&upsName, *sampleInterval)
+		go sampleUps(c, upsName, *sampleInterval)
 	}
 
 	http.ListenAndServe(listenAddr, nil)
 
 }
 
-func sampleUps(upsName *string, sampleInterval int) {
-	go func() {
+func sampleUps(c chan string, upsName string, sampleInterval int) {
+	for {
 
-		for {
+		log.Info("In ", upsName)
+		cmd := exec.Command(*upscPath, upsName)
+		stdout, err := cmd.StdoutPipe()
 
-			cmd := exec.Command(*upscPath, *upsName)
-			stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Error("Error while using upsc for ", upsName, ": error piping stdout. ", upsName, " will now be ignored ")
+			return
+		}
+		if err := cmd.Start(); err != nil {
+			log.Error("Error while using upsc for ", upsName, ": error executing command. ", upsName, " will now be ignored ")
+			return
+		}
 
-			if err != nil {
-				log.Error("Error while using upsc for ", *upsName, ": error piping stdout. ", *upsName, " will now be ignored ")
-				return
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			parts := strings.Split(scanner.Text(), ": ")
+
+			if ignoredKey(parts[0]) {
+				log.Debug("the key ", parts[0], " is ignored")
+				continue
 			}
-			if err := cmd.Start(); err != nil {
-				log.Error("Error while using upsc for ", *upsName, ": error executing command. ", *upsName, " will now be ignored ")
-				return
-			}
 
-			scanner := bufio.NewScanner(stdout)
-			for scanner.Scan() {
-				parts := strings.Split(scanner.Text(), ": ")
+			// if we can convert to float assign a value
+			// if not, just check for ups status (OL || OB)
 
-				if ignoredKey(parts[0]) {
-					log.Debug("the key ", parts[0], " is ignored")
+			if parts[0] == "ups.status" {
+				gauge := getGauge(parts[0])
+				if gauge == nil {
+					log.Info(upsName, " ask key creation ")
+					c <- parts[0]
 					continue
 				}
-
-				// if we can convert to float assign a value
-				// if not, just check for ups status (OL || OB)
-
-				if parts[0] == "ups.status" {
-					gauge := CreateAndRegisterGauge(*upsName, parts[0], prometheusMetrics[*upsName])
-					//TODO: Check and apply each status number
-					switch parts[1] {
-					case "CAL":
-						gauge.Set(0)
-					case "TRIM":
-						gauge.Set(1)
-					case "BOOST":
-						gauge.Set(2)
-					case "OL":
-						gauge.Set(3)
-					case "OB":
-						gauge.Set(4)
-					case "OVER":
-						gauge.Set(5)
-					case "LB":
-						gauge.Set(6)
-					case "RB":
-						gauge.Set(7)
-					case "BYPASS":
-						gauge.Set(8)
-					case "OFF":
-						gauge.Set(9)
-					case "CHRG":
-						gauge.Set(10)
-					case "DISCHRG":
-						gauge.Set(11)
-					}
-				} else {
-					value, err := strconv.ParseFloat(parts[1], 64)
-					if err == nil {
-						gauge := CreateAndRegisterGauge(*upsName, parts[0], prometheusMetrics[*upsName])
-						gauge.Set(value)
-					}
+				//TODO: Check and apply each status number
+				switch parts[1] {
+				case "CAL":
+					gauge.WithLabelValues(upsName).Set(0)
+				case "TRIM":
+					gauge.WithLabelValues(upsName).Set(1)
+				case "BOOST":
+					gauge.WithLabelValues(upsName).Set(2)
+				case "OL":
+					gauge.WithLabelValues(upsName).Set(3)
+				case "OB":
+					gauge.WithLabelValues(upsName).Set(4)
+				case "OVER":
+					gauge.WithLabelValues(upsName).Set(5)
+				case "LB":
+					gauge.WithLabelValues(upsName).Set(6)
+				case "RB":
+					gauge.WithLabelValues(upsName).Set(7)
+				case "BYPASS":
+					gauge.WithLabelValues(upsName).Set(8)
+				case "OFF":
+					gauge.WithLabelValues(upsName).Set(9)
+				case "CHRG":
+					gauge.WithLabelValues(upsName).Set(10)
+				case "DISCHRG":
+					gauge.WithLabelValues(upsName).Set(11)
 				}
+			} else {
+				value, err := strconv.ParseFloat(parts[1], 64)
+				if err == nil {
+					gauge := getGauge(parts[0])
+					if gauge == nil {
+						log.Info(upsName, " ask key creation ")
+						c <- parts[0]
+						continue
+					}
 
-				if err := scanner.Err(); err != nil {
-					log.Info("Error while using upsc for ", *upsName, "error scanning command output ", *upsName, " will now be ignored ")
+					gauge.WithLabelValues(upsName).Set(value)
 				}
-
-				cmd.Wait() // needed to close the pipe and release the child process
-				time.Sleep(time.Duration(sampleInterval) * time.Second)
 			}
 		}
-	}()
 
+		if err := scanner.Err(); err != nil {
+			log.Info("Error while using upsc for ", upsName, "error scanning command output ", upsName, " will now be ignored ")
+		}
+
+		cmd.Wait() // needed to close the pipe and release the child process
+		time.Sleep(time.Duration(sampleInterval) * time.Second)
+
+	}
 }
 
 func ignoredKey(key string) bool {
@@ -163,22 +178,35 @@ func toPrometheusKey(s string) string {
 	return "upsc_" + strings.ReplaceAll(s, ".", "_")
 }
 
-func CreateAndRegisterGauge(upsName string, key string, m map[string]prometheus.Gauge) prometheus.Gauge {
+func CreateAndRegisterGauge(key string) prometheus.GaugeVec {
 
 	key = toPrometheusKey(key)
 
-	if val, ok := m[key]; ok {
+	if val, ok := prometheusMetrics[key]; ok {
 		return val
 	} else {
-		log.Info("Creating key " + key)
-		constLabels := map[string]string{"ups": upsName}
-		gauge := prometheus.NewGauge(prometheus.GaugeOpts{
-			Name:        key,
-			ConstLabels: constLabels,
+		log.Info("Creating key ", key)
+		gauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Name: key,
+		}, []string{
+			"ups",
 		})
 
 		prometheus.MustRegister(gauge)
-		m[key] = gauge
-		return gauge
+		prometheusMetrics[key] = *gauge
+		return *gauge
 	}
+}
+
+func processGaugeCreation(c chan string) {
+	for i := range c {
+		_ = CreateAndRegisterGauge(i)
+	}
+}
+
+func getGauge(key string) *prometheus.GaugeVec {
+	if val, ok := prometheusMetrics[toPrometheusKey(key)]; ok {
+		return &val
+	}
+	return nil
 }
